6bf5c8b69807a4e9ade3232e021441ab755b6458
Eden <you8inpark@gmail.com>
2024-11-21 00:30:30 +0900

    feature: UILabel 추가 및 속성 설정 메서드 구현 및 Snapkit을 사용해 AutoLayout 제약 조건 추가

--- calculator-app/ViewController.swift
+++ calculator-app/ViewController.swift
@@ -15,12 +15,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpView()
+        setUpNumberLabel()
+        setUpConstraints()
     }
     
     private func setUpView() {
         view.backgroundColor = .black
     }
-
-
+    
+    private func setUpNumberLabel() {
+        numberLabel.textColor = .white
+        numberLabel.text = "12345"
+        numberLabel.textAlignment = .right
+        numberLabel.font = UIFont.boldSystemFont(ofSize: 60)
+        
+        view.addSubview(numberLabel)
+    }
+    
+    private func setUpConstraints() {
+        numberLabel.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(30)
+            $0.top.equalToSuperview().offset(200)
+            $0.height.equalTo(100)
+        }
+    }
 }
 
